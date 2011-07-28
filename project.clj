77077d651a7d5fe57b54baa9a7cf1ce8ce77b308
Sam Ritchie <sritchie09@gmail.com>
2011-07-27 23:36:14 -0400

    Updated cascalog dependency.

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject midje-cascalog "0.1.0-SNAPSHOT"
+(defproject midje-cascalog "0.1.0"
   :description "Cascalog functions for Midje."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojars.sritchie09/cascalog "1.7.2"]
-                 ;; [cascalog "1.8.0-SNAPSHOT"]
+                 [cascalog "1.8.0"]
                  [midje "1.2-alpha3"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [swank-clojure "1.4.0-SNAPSHOT"]
