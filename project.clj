7fa91cb56f37677f006d775c1e4a1e553a883246
Sam Ritchie <sritchie09@gmail.com>
2011-10-14 10:19:40 -0700

    Bumped versions in project.clj in expectation of a midje-cascalog upgrade.

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
-(defproject midje-cascalog "0.2.2"
+(defproject midje-cascalog "0.3.0-SNAPSHOT"
   :description "Cascalog functions for Midje."
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [cascalog "1.8.2"]
-                 [midje "1.2.0"]]
+                 [midje "1.3-alpha4"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [lein-midje "1.0.3"]
-                     [swank-clojure "1.4.0-SNAPSHOT"]
-                     [clojure-source "1.2.0"]])
+                     [swank-clojure "1.4.0-SNAPSHOT"]])
