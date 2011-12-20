6b1dfd7d2cb949eefdb52070f0c7e1315d44eeb5
Sam Ritchie <sritchie09@gmail.com>
2011-12-19 19:28:08 -0500

    Moved plugins out of project. Upgraded midje dependency.

--- project.clj
+++ project.clj
@@ -2,11 +2,9 @@
   :description "Cascalog functions for Midje."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [cascalog "1.8.4"]
-                 [midje "1.3-alpha5"]]
+                 [midje "1.3.0"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [lein-midje "1.0.4"]
-                     [swank-clojure "1.4.0-SNAPSHOT"]
                      [lein-multi "1.1.0-SNAPSHOT"]]
   :multi-deps {"1.9" [[org.clojure/clojure "1.3.0"]
                       [cascalog "1.9.0-wip"]
-                      [midje "1.3-alpha5"]]})
+                      [midje "1.3.0"]]})
